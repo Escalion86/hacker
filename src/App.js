ab1dafd9730326524676586b90783e3f4cbc24aa
Escalion <escalion86@gmail.com>
2024-05-09 16:19:37 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1476,8 +1476,10 @@ function App() {
       {isConnected && (
         <div className="absolute left-0 top-0 h-[2px] w-[2px] bg-gray-500" />
       )}
-      <div className="absolute z-50 left-0 bottom-0 max-h-[100px] h-[100px] right-0 bg-white text-black text-xs">
-        {logs}
+      <div className="p-1 absolute z-50 left-0 bottom-0 max-h-[100px] h-[100px] right-0 bg-white text-black text-xs">
+        {logs.map((log, index) => (
+          <div key={index}>{log}</div>
+        ))}
       </div>
       {JSON.stringify(wifiSpots)}
       <div className="flex px-2 py-1 bg-black">
