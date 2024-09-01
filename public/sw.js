6e33155aabcfc9cdf750590ed2b8c9fb88198639
Escalion <escalion86@gmail.com>
2024-09-01 14:43:54 +0700

    Added work in offline

--- public/sw.js
+++ public/sw.js
@@ -3,12 +3,13 @@ this.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(cacheData).then((cache) => {
       cache.addAll([
-        'static/js/bundle.js',
-        '/index.html',
-        '/',
-        '/manifest.json',
-        '/favicon.ico',
-        '/static/js/',
+        // 'static/js/bundle.js',
+        // '/index.html',
+        // '/',
+        // '/manifest.json',
+        // '/favicon.ico',
+        // '/static/js/',
+        './',
       ])
     })
   )
